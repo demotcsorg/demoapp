c0e2d4ef481824a6c7662b6bf6aacc2f12aa1a85
Saurabh Rai <42387065+SaurabhRai19@users.noreply.github.com>
2021-08-23 12:54:08 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-sampleapp';
+  var h1 = document.querySelector('h1');
+  h1.innerHTML = '<a href="&#x3000;javascript:alert(1)">CLICKME</a>';
+  var innerHTML = h1.innerHTML;
+  console.log(innerHTML);
+  h1.innerHTML = innerHTML;
 }
